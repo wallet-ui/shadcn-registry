05f39e0a241f7f65eee25cb699b09d7126abd228
Bram Borggreve <beeman@beeman.nl>
2025-09-05 07:17:49 +0100

    feat: add support for Solana Mobile Wallet Adapter

--- registry/new-york/blocks/solana-provider/solana-provider.tsx
+++ registry/new-york/blocks/solana-provider/solana-provider.tsx
@@ -9,10 +9,13 @@ import {
   WalletUi
 } from '@wallet-ui/react'
 import { WalletUiGillProvider } from '@wallet-ui/react-gill'
+import { solanaMobileWalletAdapter } from "./solana-mobile-wallet-adapter";
 
 const config = createWalletUiConfig({
   clusters: [createSolanaDevnet(), createSolanaLocalnet(), createSolanaTestnet()],
 })
+
+solanaMobileWalletAdapter({ clusters: config.clusters })
 
 export function SolanaProvider({ children }: { children: ReactNode }) {
   return <WalletUi config={config}>
