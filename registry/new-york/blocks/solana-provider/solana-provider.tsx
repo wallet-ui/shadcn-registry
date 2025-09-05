dcf574197e2d9f265d990190995320970a7a2234
Bram Borggreve <beeman@beeman.nl>
2025-09-05 05:33:33 +0100

    fix: remove unused dynamic buttons from providers

--- registry/new-york/blocks/solana-provider/solana-provider.tsx
+++ registry/new-york/blocks/solana-provider/solana-provider.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import dynamic from 'next/dynamic'
 import { ReactNode } from 'react'
 import {
   createSolanaDevnet,
@@ -10,13 +9,6 @@ import {
   WalletUi
 } from '@wallet-ui/react'
 import { WalletUiGillProvider } from '@wallet-ui/react-gill'
-
-export const WalletButton = dynamic(async () => (await import('@wallet-ui/react')).WalletUiDropdown, {
-  ssr: false,
-})
-export const ClusterButton = dynamic(async () => (await import('@wallet-ui/react')).WalletUiClusterDropdown, {
-  ssr: false,
-})
 
 const config = createWalletUiConfig({
   clusters: [createSolanaDevnet(), createSolanaLocalnet(), createSolanaTestnet()],
