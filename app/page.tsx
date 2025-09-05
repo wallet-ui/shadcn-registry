f8c4ac8796f8ebadbf45fa53a7078c86aa166dd0
Bram Borggreve <beeman@beeman.nl>
2025-09-05 11:00:27 +0100

    docs: suggest tweakcn for theming

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ import { useQuery } from "@tanstack/react-query";
 export default function Home() {
   return (
     <div className="max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8">
-      <header className="flex flex-col gap-1">
+      <header className="flex flex-col gap-2">
         <h1 className="text-3xl font-bold tracking-tight">Wallet UI Shadcn Registry</h1>
         <p className="text-muted-foreground">
           The <a target="_blank" rel="noopener noreferrer" className="underline" href='https://ui.shadcn.com/docs/registry'>Shadcn registry</a>{' '}
@@ -30,6 +30,10 @@ export default function Home() {
         </p>
         <p className="text-muted-foreground">
           Support for Solana Mobile comes out of the box!
+        </p>
+        <p className="text-muted-foreground">
+          Head over to <a target="_blank" rel="noopener noreferrer" className="underline" href='https://tweakcn.com/editor/theme'>tweakcn</a>{' '}
+          to theme your components!
         </p>
       </header>
       <main className="flex flex-col flex-1 gap-8">
