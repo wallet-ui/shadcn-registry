78282df72e19d3b7ef16a368df0cc1c97d305a1b
Bram Borggreve <beeman@beeman.nl>
2025-09-05 05:30:25 +0100

    feat: add demo with balance fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import * as React from "react"
 import { OpenInV0Button } from "@/components/open-in-v0-button"
 import { ClusterDropdown } from "@/registry/new-york/blocks/cluster-dropdown/cluster-dropdown"
@@ -11,6 +12,10 @@ import {
 import { InstallationInstallPackage } from "@/components/installation-install-package";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { LucideBird } from "lucide-react";
+import { address, lamportsToSol } from "gill";
+import { useSolana } from "@/registry/new-york/blocks/solana-provider/use-solana";
+import { WalletUiAccountGuard, WalletUiAccountInfo } from "@wallet-ui/react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   return (
@@ -36,6 +41,17 @@ export default function Home() {
             </p>
           </AlertDescription>
         </Alert>
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              Demo: select wallet and cluster to fetch your balance.
+            </h2>
+            <OpenInV0Button name="wallet-dropdown" className="w-fit" />
+          </div>
+          <div className="flex flex-col items-center justify-center min-h-[400px] relative">
+            <WalletUiAccountGuard fallback={<WalletDropdown />} render={(props) => <AccountBalance {...props}  />}/>
+          </div>
+        </div>
         <h2 className="text-2xl font-bold tracking-tight">Getting started</h2>
         <h3 className="text-xl font-semibold">1. Configure your shadcn config</h3>
         <p className="text-foreground">
@@ -80,7 +96,6 @@ export default function Home() {
             <ClusterDropdown />
           </div>
         </div>
-
         <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
@@ -88,18 +103,32 @@ export default function Home() {
             </h2>
             <OpenInV0Button name="wallet-dropdown" className="w-fit" />
           </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
+          <div className="flex flex-col items-center justify-center min-h-[400px] relative">
             <WalletDropdown />
           </div>
         </div>
-
-
-
-
-
-
-
       </main>
     </div>
   )
+}
+
+
+function useBalanceQuery({account, accountKeys}: WalletUiAccountInfo) {
+  const { client } = useSolana()
+
+  return useQuery({
+    retry: false,
+    queryKey: accountKeys,
+    queryFn: () => client.rpc.getBalance(address(account.address)).send(),
+  })
+}
+
+function AccountBalance(props: WalletUiAccountInfo) {
+  const query = useBalanceQuery(props)
+  const balance = query.data?.value?.toString()
+  return <div className="flex items-center justify-center relative gap-2">
+    <WalletDropdown />
+    <ClusterDropdown />
+    {query.isLoading ? 'Loading...' : query.isError ? `${query.error}` : balance ? `${lamportsToSol(Number.parseInt(balance))} SOL` : '?'}
+  </div>
 }
--- components/providers.tsx
+++ components/providers.tsx
@@ -2,9 +2,12 @@
 
 import React from 'react'
 import { SolanaProvider } from '@/registry/new-york/blocks/solana-provider/solana-provider'
+import { ReactQueryProvider } from "@/components/react-query-provider";
 
 export function AppProviders({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <ReactQueryProvider>
       <SolanaProvider>{children}</SolanaProvider>
+    </ReactQueryProvider>
   )
 }
