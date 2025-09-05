6e426316f8b20f553d4904fe2a977a5f5f5b79fd
Bram Borggreve <beeman@beeman.nl>
2025-09-05 06:46:49 +0100

    feat: add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import { AppProviders } from "@/components/providers";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <AppProviders>
           {children}
+          <Analytics />
         </AppProviders>
       </body>
     </html>
