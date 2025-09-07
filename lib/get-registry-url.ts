8a923064715d089a59411ede35d48aae88c85e75
Bram Borggreve <beeman@beeman.nl>
2025-09-07 14:13:03 +0100

    fix: getRegistryBaseUrl order

--- lib/get-registry-url.ts
+++ lib/get-registry-url.ts
@@ -3,9 +3,8 @@ function env(key: string) {
 }
 
 export function getRegistryBaseUrl() {
-  console.log(Object.keys(process.env).sort().map(k => ` [${k}] ${process.env[k as keyof typeof process.env]}`))
   const registryUrl = env('REGISTRY_URL')
   const vercelUrl = env('VERCEL_URL')
 
-  return vercelUrl ? `https://${vercelUrl}` : registryUrl ?? `http://localhost:3000`
+  return registryUrl ? registryUrl : vercelUrl ? `https://${vercelUrl}` : `http://localhost:3000`
 }
